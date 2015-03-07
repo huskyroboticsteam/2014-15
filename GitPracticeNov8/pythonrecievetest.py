f36823b0a258bd6e8a9ecc2d6835ba3aa92a781e
Mightymaster123 <hewittm1@yahoo.com>
2015-03-07 11:50:24 -0800

    Fixed Print
    Print updated

--- GitPracticeNov8/pythonrecievetest.py
+++ GitPracticeNov8/pythonrecievetest.py
@@ -9,4 +9,4 @@ sock.bind((UDP_IP, UDP_PORT))
    
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    print ("received message:"), data
