d3f870a17cd8fcaf8bc388c2c6e1a24c8d7efea4
Thomas Pryor <tommy.pryor1@gmail.com>
2015-05-15 18:47:38 -0700

    Create armControl.ino

---
+++ Arduino Code/masterCode/armControl.ino
@@ -0,0 +1,30 @@
+// Apr 18, 2015. Husky Robotics, made by Electrical Team
+// Using Analog read from current sensor ACS758
+
+// Attach the arm motors
+void attachArmMotors()
+{
+    for (int i = 0; i < 3; i++) {
+        armMotor[i].attach(TALON_ARM[i]);
+    }
+}
+
+// writes position from wireless to the arm motors
+void writeToArmMotors()
+{
+    for(int i = 0; i < 3; i++) {
+        armMotor[i].writeMicroseconds(dirToMicroseconds(arm[i]));
+    }
+}
+
+// DEMO
+void armRiseAndBack() {
+      for(int i = 0; i < 3; i++) {
+        armMotor[i].writeMicroseconds(SPEEDUP_FREQUENCY);
+        Serial.println(TALON_ARM[i]);
+        delay(5000);
+        armMotor[i].writeMicroseconds(SPEEDDOWN_FREWQUENCY);
+        delay(5000);
+        armMotor[i].writeMicroseconds(NEUTRAL_FREQUENCY);
+      }
+}
