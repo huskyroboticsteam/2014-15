08fd6a2926514686bfde5837362cee741e39a16f
Thomas Pryor <tommy.pryor1@gmail.com>
2015-05-21 21:06:26 -0700

    Update masterCode.ino

--- Arduino Code/masterCode/masterCode.ino
+++ Arduino Code/masterCode/masterCode.ino
@@ -9,15 +9,20 @@
 
 void setup()
 {
+    Serial.println("STARTING");
     attachDriveMotors();
     attachArmMotors();
+    attachHandMotors();
     initializeWireless();
 }
 
 void loop()
 {
+    overCurrentCheck();
     timeoutCheck();
+    compressorPressureCheck();    
     receiveWirelessData();
     writeToDriveMotors();
     writeToArmMotors();
+    writeToHandMotors();
 }
